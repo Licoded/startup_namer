f44fb41eec8d1e5ca6e5de8fb6e9d03daaf07255
Licoded <busy.li@foxmail.com>
2021-02-20 08:43:44 +0800

    Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
-        body: Center(
-          child: RandomWords(),
-        ),
+        body: RandomWords(),
       ),
     );
   }
@@ -35,9 +33,41 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          // The iterator begins at 0 and increments each time the function is called, once for every suggested word pairing.
+          if (i.isOdd) {
+            return Divider();
+          }
+          /*
+            The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+            For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+            This calculates the actual number of word pairings in the ListView,minus the divider widgets.
+          */
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word pairings...
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return _buildSuggestions();
   }
 }
