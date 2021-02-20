4059b037fa1a4920ada3f3f609a19e600496807c
Licoded <busy.li@foxmail.com>
2021-02-20 08:09:10 +0800

    Add a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,28 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+/*
+ In your IDE, start typing stful.
+ The editor asks if you want to create a Stateful widget. Press Return to accept.
+ The boilerplate code for two classes appears, and the cursor is positioned for you to enter the name of your stateless widget.
+ Enter RandomWords as the name of your widget.
+ */
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
